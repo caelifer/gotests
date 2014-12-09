26e7d346862b7f3ccb9ea6c17d2502d33c3d2570
Timour Ezeev <timour.ezeev@me.com>
2014-12-09 18:00:09 -0500

    Refactored a bit and set number of workers to 512 based on a small experiment.

--- fstree/cmd/search/search.go
+++ fstree/cmd/search/search.go
@@ -1,23 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
-	"runtime"
+	"path/filepath"
 
 	"github.com/caelifer/dups/balancer"
 	"github.com/caelifer/gotests/fstree"
 )
 
+var (
+	// command line flags
+	maxWorkers = flag.Int("jobs", 512, "Number of parallel threads")
+)
+
 func init() {
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetFlags(log.Ldate | log.Ltime)
+	log.SetPrefix(filepath.Base(os.Args[0]))
 }
 
 func main() {
-	for _, root := range os.Args[1:] {
-		// err := fstree.Walk(balancer.NewWorkQueue(runtime.NumCPU()*4), root, func(path string, dent fstree.Dirent, err error) error {
-		err := fstree.Walk(balancer.NewWorkQueue(runtime.NumCPU()), root, func(path string, dent fstree.Dirent, err error) error {
+	flag.Parse()
+
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	for _, root := range roots {
+		err := fstree.Walk(balancer.NewWorkQueue(*maxWorkers), root, func(path string, dent fstree.Dirent, err error) error {
 			fmt.Printf("[%s] %s\n", dent.Type(), path)
 			return nil
 		})

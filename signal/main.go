ee8dfdf9874dc8eebaca283959154b823af5d1be
Timour Ezeev <timour.ezeev@me.com>
2014-12-16 12:42:30 -0500

    Use more elaborate signal handling strategy using new API to safely unregister handlers

--- signal/main.go
+++ signal/main.go
@@ -15,14 +15,26 @@ func main() {
 	dispatch.HandleSignal(os.Interrupt, handleSIGINT_ONE)
 
 	periodic := time.Tick(1 * time.Second)
+	start := time.Now()
+
 	for _ = range periodic {
 		fmt.Print(".")
+		if time.Since(start) > 5*time.Second {
+			dispatch.StopSignalHandler(os.Interrupt)
+			time.Sleep(3 * time.Second)
+			break // for loop
+		}
 	}
+	os.Exit(0)
 }
 
 func handleSIGINT_ONE(signal os.Signal) {
 	sigCounter++
 	fmt.Printf("\nHandler ONE: Got signal [%v]\n", signal)
+
+	if sigCounter > 3 {
+		os.Exit(1)
+	}
 
 	// Install different signal in mid-flight
 	dispatch.HandleSignal(os.Interrupt, handleSIGINT_TWO)
@@ -31,7 +43,11 @@ func handleSIGINT_ONE(signal os.Signal) {
 func handleSIGINT_TWO(signal os.Signal) {
 	sigCounter++
 	fmt.Printf("\nHandler TWO: Got signal [%v]\n", signal)
-	if sigCounter > 1 {
+
+	if sigCounter > 3 {
 		os.Exit(1)
 	}
+
+	// Install different signal in mid-flight
+	dispatch.HandleSignal(os.Interrupt, handleSIGINT_ONE)
 }

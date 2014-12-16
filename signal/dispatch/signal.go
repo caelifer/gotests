4f687899f3d475e5e6316b73752217246c7ae6de
Timour Ezeev <timour.ezeev@me.com>
2014-12-16 11:53:39 -0500

    Refactor signal dispatch into its own package

---
+++ signal/dispatch/signal.go
@@ -0,0 +1,58 @@
+package dispatch
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+)
+
+// dispatcher
+var dispatcher = struct {
+	*sync.Mutext
+	signals map[os.Signal]chan os.Signal
+}{
+	new(sync.Mutex),
+	make(map[os.Signal]chan os.Signal),
+}
+
+// SignalHandler is a custom function that handles os.Signal
+type SignalHandler func(os.Signal)
+
+// HandleSignal installs custom SignalHandler handler for a particular os.Signal
+// provided by sig argument.
+func HandleSignal(sig os.Signal, handler SignalHandler) {
+	// Take exclusive lock
+	dispatcher.Lock()
+
+	// Check if we already have registered handler
+	if ch, ok := dispatcher.signals[sig]; ok {
+		// Signal handler already exists - do clean-up
+		log.Printf("clean-up existing [%s] handler", sig)
+
+		// Stop receiving signlas
+		signal.Stop(ch)
+		// Close signal channel so gorutine can safely exit
+		close(ch)
+	}
+
+	log.Printf("registering new [%s] handler", sig)
+
+	// Create buffered channel of os.Signal values
+	ch = make(chan os.Signal, 1)
+	// Store our new channel
+	dispatcher.signals[sig] = ch
+
+	// Unlock
+	dispatcher.Unlock()
+
+	// Set notification
+	signal.Notify(ch, sig)
+
+	// Install custom handler in the separate gorutine
+	go func(c <-chan os.Signal) {
+		for s := range c {
+			handler(s)
+		}
+	}(ch)
+}

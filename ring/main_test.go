39f31eda9e1ebddf3d2509070382cf43d4d77c32
Timour Ezeev <timour.ezeev@me.com>
2015-03-25 18:00:15 -0400

    Added benchmark cases for differnt number of messages

--- ring/main_test.go
+++ ring/main_test.go
@@ -15,9 +15,33 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func BenchmarkRing(b *testing.B) {
+func BenchmarkRing_____1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		send(in, 1)
+		recv(out, 1)
+	}
+}
+func BenchmarkRing____10(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		send(in, 10)
+		recv(out, 10)
+	}
+}
+func BenchmarkRing___100(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		send(in, 100)
+		recv(out, 100)
+	}
+}
+func BenchmarkRing__1000(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		send(in, 1000)
 		recv(out, 1000)
+	}
+}
+func BenchmarkRing_10000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		send(in, 10000)
+		recv(out, 10000)
 	}
 }

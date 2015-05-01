71d555989e6fb348317e327bef38e0670d89e79e
Timour Ezeev <timour.ezeev@me.com>
2015-05-01 19:40:41 -0400

    Made test more verbose

--- json/main_test.go
+++ json/main_test.go
@@ -34,23 +34,24 @@ var tests = []struct {
 		},
 		{
 			"of1": "of1val2",
-			"of2": "of2val2",
-			"of3": "of3val2"
+			"of2": "of2val2"
 		}
 	]
 `,
 		meta: "arrayOfObjects",
-		want: "{of1:of1val1,of2:of2val1,of3:of3val1},{of1:of1val2,of2:of2val2,of3:of3val2}",
+		want: "{of1:of1val1,of2:of2val1,of3:of3val1},{of1:of1val2,of2:of2val2}",
 	},
 }
 
 func TestCustJSONParsing(t *testing.T) {
-	// Parse our test object
+	// Run out test cases
 	for _, tst := range tests {
 		got := parser.Parse(tst.meta, tst.test)
 
 		if tst.want != got {
 			t.Errorf("Got: %q, expected: %q\n", got, tst.want)
+		} else {
+			t.Logf("[PASS] got %q while parsing %s", got, tst.meta)
 		}
 	}
 }

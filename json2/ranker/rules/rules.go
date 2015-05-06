249a161cc68ced144d3a64ece5afc98359e884da
Timour Ezeev <timour.ezeev@me.com>
2015-05-05 21:19:26 -0400

    Added rules.Any() function

--- json2/ranker/rules/rules.go
+++ json2/ranker/rules/rules.go
@@ -6,20 +6,32 @@ type Rule interface {
 }
 
 func Not(r Rule) Rule {
-	assert := func(cond interface{}) bool {
-		return !r.Assert(cond)
-	}
-	return MakeRule(r.Name(), assert)
+	return MakeRule(
+		r.Name(),
+		func(cond interface{}) bool {
+			return !r.Assert(cond)
+		},
+	)
 }
 
 func All(cond interface{}, rules ...Rule) bool {
 	res := false
 	for _, r := range rules {
+		// log.Printf("Processing rule: %v", r.Name())
 		if res = r.Assert(cond); !res {
 			return res
 		}
 	}
 	return res
+}
+
+func Any(cond interface{}, rules ...Rule) bool {
+	for _, r := range rules {
+		if r.Assert(cond) {
+			return true
+		}
+	}
+	return false
 }
 
 func MakeRule(name string, assert func(interface{}) bool) Rule {

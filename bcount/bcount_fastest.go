50d15b725406bfe8c494a76c9355d204188b8f65
Timour Ezeev <timour.ezeev@me.com>
2015-05-12 06:59:38 -0400

    Got rid off helper function for fastest algo

--- bcount/bcount_fastest.go
+++ bcount/bcount_fastest.go
@@ -11,17 +11,8 @@ var bits = []int{
 	3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8,
 }
 
-// fastestCountBitsInUint8 counts set bits byte represented as uint8
-func fastestCountBitsInUint8(b byte) int {
-	// Return results from pre-calculated table
-	return bits[b]
-}
-
 // fastestCountSetBitsInUint32 counts bits in provided uint32 by
 // summing bit counts of 4 bytes that comprises uint32
 func fastestCountSetBitsInUint32(ui uint32) int {
-	return fastestCountBitsInUint8(byte(ui>>24)) +
-		fastestCountBitsInUint8(byte(ui>>16)) +
-		fastestCountBitsInUint8(byte(ui>>8)) +
-		fastestCountBitsInUint8(byte(ui))
+	return bits[byte(ui>>24)] + bits[byte(ui>>16)] + bits[byte(ui>>8)] + bits[byte(ui)]
 }

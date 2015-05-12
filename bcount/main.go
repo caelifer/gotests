ffddf01132936e040640943db621b02326476665
Timour Ezeev <timour.ezeev@me.com>
2015-05-12 04:43:33 -0400

    Use fastest algorithm in main.go

--- bcount/main.go
+++ bcount/main.go
@@ -6,24 +6,24 @@ func main() {
 	// First 15 elems
 	for i := uint32(0); i < 0x10; i++ {
 		j := uint32(i + 0x0)
-		fmt.Printf("%10d %032b %d\n", j, j, fastCountSetBitsInUint32(j))
+		fmt.Printf("%10d %032b %d\n", j, j, fastestCountSetBitsInUint32(j))
 	}
 
 	// Second set
 	for i := uint32(0); i < 0x10; i++ {
 		j := uint32(i + 0xFF0)
-		fmt.Printf("%10d %032b %d\n", j, j, fastCountSetBitsInUint32(j))
+		fmt.Printf("%10d %032b %d\n", j, j, fastestCountSetBitsInUint32(j))
 	}
 
 	// Third set
 	for i := uint32(0); i < 0x10; i++ {
 		j := uint32(i + 0xFFFF0)
-		fmt.Printf("%10d %032b %d\n", j, j, fastCountSetBitsInUint32(j))
+		fmt.Printf("%10d %032b %d\n", j, j, fastestCountSetBitsInUint32(j))
 	}
 
 	// Last 15 elems
 	for i := uint32(0); i < 0x10; i++ {
 		j := uint32(i + 0xFFFFFFF0)
-		fmt.Printf("%10d %032b %d\n", j, j, fastCountSetBitsInUint32(j))
+		fmt.Printf("%10d %032b %d\n", j, j, fastestCountSetBitsInUint32(j))
 	}
 }
